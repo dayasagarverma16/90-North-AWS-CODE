3946b14c4d2055447105bf7442409aead9ecd30a
Your Correct GitHub Username <your-email@example.com>
2025-01-18 19:23:59 +0530

    Add existing project files to Git

---
+++ sum.py
@@ -0,0 +1,7 @@
+
+LamdaFunction = lambda x,y:x+y
+  
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))   
+
+print("the sum of ",num1,"and ",num2,"is:::",LamdaFunction(num1,num2)) 
